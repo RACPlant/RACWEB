783e784d319bd25696fb463e3a3d522d25a1dc02
jpedrojpedro <joaopedro.pinheiro88@gmail.com>
2019-11-12 15:21:03 -0300

    Atualizando contrato de retorno da API

--- app/views.py
+++ app/views.py
@@ -6,46 +6,59 @@ from flask import abort, jsonify
 def welcome():
     return jsonify({'msg': 'Bem vindos!'})
 
-
-@app.route("/arduino/", methods=['GET'])
-def list_arduino():
+# TODO: criar endpoint referente ao raspberry
+#
+# 'position': 'front-yard',  # lat long?
+# 'label': 'Front Yard Station',
+# 'started_at': '2019-11-05 10:50:31'
+#
+# 'position': 'back-yard',  # lat long?
+# 'label': 'Front Yard Station',
+# 'started_at': '2019-11-05 10:50:31'
+@app.route("/raspberry/<path:rasp_id>/arduino/", methods=['GET'])
+def list_arduino(rasp_id):
+    raspberry_id = __raspberry_params(rasp_id)
+    # TODO: filter by specific raspberry_id
     return jsonify(
         [
             {
                 'ard-01': {
-                    'position': 'front-yard',  # lat long?
-                    'label': 'Front Yard Station',
-                    'started_at': '2019-11-05 10:50:31'
+                    'id': '0',
+                    'port': '/dev/tty/ACM0',
+                    'radiation': 'r1',
+                    'temperature': 't1'
                 }
             },
             {
                 'ard-02': {
-                    'position': 'back-yard',  # lat long?
-                    'label': 'Front Yard Station',
-                    'started_at': '2019-11-05 10:50:31'
+                    'id': '1',
+                    'port': '/dev/tty/ACM1',
+                    'radiation': 'r2',
+                    'temperature': 't2'
                 }
             }
         ]
     )
 
 
-@app.route("/arduino/<path:ard_id>/plants/", methods=['GET'])
-def list_plants(ard_id):
+@app.route("/raspberry/<path:rasp_id>/arduino/<path:ard_id>/slots/", methods=['GET'])
+def list_slots(rasp_id, ard_id):
+    raspberry_id = __raspberry_params(rasp_id)
     arduino_id = __arduino_params(ard_id)
-    # TODO: access data from specified arduino and retrieve associated plants
+    # TODO: access data from specified arduino and retrieve associated slots
     return jsonify(
         [
             {
-                'plant-01': {
-                    'type': 'Capsicum frutescens',
-                    'name': 'Pimenteira'
-                }
+                'botanical-name': 'Abelia chinensis',
+                'name': 'Pimenteira',
+                'pump': 'p1',
+                'humidity': 'h1'
             },
             {
-                'plant-02': {
-                    'type': 'Nephrolepis Exaltata',
-                    'name': 'Samambaia'
-                }
+                'botanical-name': 'Abelia floribunda',
+                'name': 'Samambaia',
+                'pump': 'p2',
+                'humidity': 'h2'
             }
         ]
     )
@@ -56,3 +69,10 @@ def __arduino_params(ard_id):
         abort(404)
         return
     return int(ard_id)
+
+
+def __raspberry_params(rasp_id):
+    if not rasp_id or not rasp_id.isnumeric():
+        abort(404)
+        return
+    return int(rasp_id)
